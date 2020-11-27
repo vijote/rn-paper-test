4d5db33e3ec99dd623c72362aa3ee5667c900f1e
Juan Ignacio Dominguez <juanignaciodom3@gmail.com>
2020-11-27 17:57:50 -0300

    Added moment, imported AppBar and SearchBar

--- App.js
+++ App.js
@@ -1,21 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { Appbar, Searchbar } from 'react-native-paper';
+import moment from 'moment';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#3d3d3d',
+  },
+};
 
 export default function App() {
+  const date = moment().format('dddd, MMMM Do');
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = query => setSearchQuery(query);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <SafeAreaView style={styles.container}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+        <Text style={{color: '#fefefe'}}>Open up App.js to start working on your app!</Text>
+        <Text>{date}</Text>
+        <Appbar style={styles.bottom}>
+          <Appbar.Action
+            icon="archive"
+            onPress={() => console.log('Pressed archive')}
+            />
+              <Appbar.Action icon="mail" onPress={() => console.log('Pressed mail')} />
+              <Appbar.Action icon="label" onPress={() => console.log('Pressed label')} />
+              <Appbar.Action
+                icon="delete"
+                onPress={() => console.log('Pressed delete')}
+              />
+        </Appbar>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    paddingTop: 30,
+    backgroundColor: '#707070',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+  bottom: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'space-between'
   },
 });
