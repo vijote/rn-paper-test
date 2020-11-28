9af64123c9270f881ce95ba5e371c87781b49a63
Juan Ignacio Dominguez <juanignaciodom3@gmail.com>
2020-11-28 17:02:34 -0300

    Added rn-elements, Modal, Image and SearchBar onSubmit

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View} from 'react-native';
+import { Button, DefaultTheme, Modal, Provider as PaperProvider } from 'react-native-paper';
 import { Appbar, Searchbar } from 'react-native-paper';
+import { Icon, Image } from 'react-native-elements'
+import axios from 'axios';
 import moment from 'moment';
 import LightingIcon from './components/LightingIcon';
 import RainIcon from './components/RainIcon';
@@ -18,25 +20,52 @@ const theme = {
 
 export default function App() {
   const date = moment().format('dddd, MMMM Do');
-  const [searchQuery, setSearchQuery] = React.useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+  const [toggleSearch, setToggleSearch] = useState(false);
+  const [weather, setWeather] = useState({});
 
   const onChangeSearch = query => setSearchQuery(query);
+
+  const searchCity = async () => {
+    const apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${searchQuery.toLowerCase()}&appid=c31be7507dbc0c604de53a9fadb835ae`;
+    try{
+      const response = await axios.get(apiUrl);
+      setWeather(response.data);
+      setSearchQuery('');
+      setToggleSearch(!toggleSearch);
+      console.log(response.data);
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  const showSearchBar = () => {
+    setToggleSearch(!toggleSearch);
+  }
   
   return (
     <PaperProvider theme={theme}>
       <View style={styles.container}>
-        <Searchbar
-          placeholder="Search"
-          onChangeText={onChangeSearch}
-          value={searchQuery}
-          style={styles.search}
-        />
+        <Modal visible={!toggleSearch}>
+          <Icon name='search' reverse color='#fefefe' iconStyle={{color: '#1d1d1d'}} onPress={showSearchBar}/>
+        </Modal>
+        <Modal visible={toggleSearch}>
+          <Searchbar
+            placeholder="Search for a city"
+            onChangeText={onChangeSearch}
+            value={searchQuery}
+            style={styles.search}
+            onSubmitEditing={searchCity}
+          />
+        </Modal>
         <View style={styles.weatherContainer}>
           <Text style={styles.date}>{date}</Text>
-          <LightingIcon/>
-          <SnowIcon/>
-          <RainIcon/>
-          <SunIcon/>
+          <Text style={styles.city}>Barranqueras</Text>
+          <Image
+            source={{uri: 'http://openweathermap.org/img/wn/03d@4x.png'}}
+            style={{ width: 200, height: 200 }}
+          />
+        <Text style={styles.temperature}>37°</Text>
         </View>
         <Appbar style={styles.bottom}>
           <Appbar.Action
@@ -59,7 +88,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 30,
-    backgroundColor: '#2d2d2d',
+    backgroundColor: '#324E71',
     alignItems: 'center',
     justifyContent: 'space-between',
   },
@@ -68,11 +97,20 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between'
   },
   date: {
-    fontSize: 20,
-    marginBottom: 20,
+    fontSize: 16,
     color: '#fefefe'
   },
   weatherContainer: {
     alignItems: 'center',
+  },
+  city: {
+    fontSize: 36,
+    color: '#fefefe',
+    marginBottom: 30
+  },
+  temperature: {
+    marginTop: 20,
+    fontSize: 60,
+    color: '#fefefe',
   }
 });
